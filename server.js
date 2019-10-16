e72d35cd35ad83c829d9de3140c86e9dd47164cb
EdSystango <nitingour99@gmail.com>
2019-10-16 12:38:58 +0530

    complete code

---
+++ server.js
@@ -0,0 +1,82 @@
+const express=require('express')
+const passport=require('passport')
+const GoogleStategy=require('passport-google-oauth20').Strategy
+const keys=require('./config/key')
+const app=express();
+const PORT=process.env.PORT || 5000
+
+const mongoose=require('mongoose',{ useUnifiedTopology: true });
+const URL="mongodb://localhost:27017/OAuthDB";
+mongoose.connect(URL, { useNewUrlParser: true, useUnifiedTopology: true },()=>{
+  console.log("MongoDB Connected");
+})
+require('./model/User')
+
+const cookieSession=require('cookie-session');
+const cookiekey='asdfdf231434'//any
+app.use(
+  cookieSession({
+//expire after 30 days
+ maxAge:30*24*60*60*1000,
+ keys:[cookiekey]
+  })
+)
+app.use(passport.initialize());
+app.use(passport.session());
+
+
+const User=mongoose.model('users');
+
+passport.serializeUser((user,done)=>{
+  done(null,user.id)
+})
+passport.deserializeUser((id,done)=>{
+User.findById(id).then((user)=>{
+  done(null,user)
+})
+})
+
+
+passport.use(
+new GoogleStategy({
+clientID:keys.googleClientID,
+clientSecret:keys.googleClientSecret,
+  callbackURL:'/auth/google/callback'
+},(token, tokenSecret, profile, done)=>{
+User.findOne({googleId:profile.id}).then((existinguser)=>{
+  if(existinguser){
+    done(null,existinguser)
+  }else{
+    new User({
+      googleId:profile.id,
+      username:profile.displayName,
+      picture:profile._json.picture
+    }).save().then((user)=>{
+      done(null,user)
+    })
+  }
+})
+ }))
+
+app.get('/auth/google',passport.authenticate('google',{scope:['profile','email']}))
+
+app.get("/",(req,res)=>{
+  res.send("Hello");
+});
+
+app.get('/auth/google/callback',passport.authenticate('google'),(req,res)=>{
+  res.redirect('/profile');
+})
+
+app.get('/api/current_user',(req,res)=>{
+res.send(req.user)
+})
+app.get('/api/logout',(req,res)=>{
+req.logout();
+//res.send(req.user);
+res.redirect('/');
+})
+
+app.listen(PORT,()=>{
+  console.log("Server started on "+PORT);
+})
