8ac14dd76529fa6794339a922fad9783f83a4021
EdSystango <nitingour99@gmail.com>
2019-10-18 12:39:27 +0530

    client updated

--- config/key.js
+++ config/key.js
@@ -1,4 +1,4 @@
 module.exports={
- googleClientID:"",
- googleClientSecret:""
+ googleClientID:"1091415061446-71pkajghoi4e9t70ed2vckrk2g4j1o1t.apps.googleusercontent.com",
+ googleClientSecret:"C-5bPPLrPVXW5jpPoasBAHJy"
 }
